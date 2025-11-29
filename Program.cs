d4a2c28c27f1652c631e4c3b530dd3701616ec76
Supawish Hanmontree <supawish35@gmail.com>
2025-11-29 17:43:11 +0700

    New project

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CalculateBMIConsoleApp;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Clear();
+        
+        Console.Write("Please enter your height in Centimeters => ");
+        float height = float.Parse(Console.ReadLine());
+        
+        Console.Write("Please enter your weight in Kilograms => ");
+        float weight = float.Parse(Console.ReadLine());
+        
+        double bmi = weight / Math.Pow(height, 2) * 10000 ;
+        
+        Console.WriteLine($"BMI: {Math.Round(bmi, 2)}");
+
+        if (bmi < 16) {
+            Console.WriteLine("Underweight (Severe Thinness)");
+        }else if (bmi < 17) {
+            Console.WriteLine("Underweight (Moderate Thinness)");
+        }else if (bmi < 18.5) {
+            Console.WriteLine("Underweight (Mild Thinness)");
+        }else if (bmi < 25) {
+            Console.WriteLine("Normal");
+        }else if (bmi < 30) {
+            Console.WriteLine("Overweight");
+        }else if (bmi < 35) {
+            Console.WriteLine("Obese Class I)");
+        }else if (bmi < 40) {
+            Console.WriteLine("Obese Class II");
+        }else {
+            Console.WriteLine("Obese Class III");
+        }
+    }
+}
